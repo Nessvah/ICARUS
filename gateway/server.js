af00c0fe0533b76954e6dbfbed36f04245566dc7
Nessvah <info@silviavanessa.dev>
2024-01-10 12:22:27 +0000

    fix eslint errors with code from new version of apollo

--- gateway/server.js
+++ gateway/server.js
@@ -1,10 +1,37 @@
-const greeting = 'Testing husky';
-//console.log(greeting);
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+// eslint-disable-next-line
+import gql from 'graphql-tag';
 
-const annabele = (arg) => {
-  //console.log(arg)
+const typeDefs = gql`
+  type Query {
+    me: User
+  }
 
-  return arg + greeting;
+  type User {
+    id: ID!
+
+    username: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me() {
+      return { id: '1', username: '@ava' };
+    },
+  },
 };
 
-annabele();
+const server = new ApolloServer({ typeDefs, resolvers });
+
+async function startServer() {
+  await startStandaloneServer(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+    listen: { port: 4000 },
+  });
+
+  //console.log(`🚀  Server ready at ${url}`);
+}
+
+startServer();
