2aba247a0782ce0632af67e67391d16e588c9e03
Nessvah <info@silviavanessa.dev>
2024-01-10 14:09:42 +0000

    add winston logger

--- gateway/server.js
+++ gateway/server.js
@@ -2,6 +2,29 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 // eslint-disable-next-line
 import gql from 'graphql-tag';
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+  );
+}
 
 const typeDefs = gql`
   type Query {
@@ -12,13 +35,14 @@ const typeDefs = gql`
     id: ID!
 
     username: String
+    number: Int
   }
 `;
 
 const resolvers = {
   Query: {
     me() {
-      return { id: '1', username: '@ava' };
+      return { id: '1', username: '@ava', number: 352152 };
     },
   },
 };
@@ -26,12 +50,12 @@ const resolvers = {
 const server = new ApolloServer({ typeDefs, resolvers });
 
 async function startServer() {
-  await startStandaloneServer(server, {
+  const { url } = await startStandaloneServer(server, {
     context: async ({ req }) => ({ token: req.headers.token }),
-    listen: { port: 4000 },
+    listen: { port: 5000 },
   });
 
-  //console.log(`🚀  Server ready at ${url}`);
+  logger.info(`🚀  Server ready at ${url}`);
 }
 
 startServer();
