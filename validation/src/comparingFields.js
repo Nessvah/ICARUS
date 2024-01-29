6a7ee64fe81c7e18b877da02b86eb52bbbf93a26
Susana Silva <su401@MSI.su401>
2024-01-29 15:16:00 +0000

    add validation

---
+++ validation/src/comparingFields.js
@@ -0,0 +1,13 @@
+/* //compare status fields
+const statusData = [
+	'delivered',
+	'pending',
+	'processing',
+	'completed',
+	'cancelled',
+];
+
+//compare roles fields
+
+const userRoleData = ['admin', 'user', 'intern'];
+ */
