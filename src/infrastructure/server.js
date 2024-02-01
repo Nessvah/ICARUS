747a70a2a3f9d905227038996a0df75b2be203b7
Nessvah <info@silviavanessa.dev>
2024-02-01 11:39:24 +0000

    remove shared folder update apollo express server to v4

--- src/infrastructure/server.js
+++ src/infrastructure/server.js
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import express from 'express';
@@ -11,17 +11,13 @@ import { auth } from './auth/auth.js';
 import { connectDB } from './db/mssql.js';
 // to ask Silvia later
 // eslint-disable-next-line node/no-unpublished-import
-import { customFormatError } from '../../shared/utils/error-handling/formatError.js';
+import { customFormatError } from '../utils/error-handling/formatError.js';
 // eslint-disable-next-line node/no-unpublished-import
-import { DatabaseError } from '../../shared/utils/error-handling/CustomErrors.js';
+import { DatabaseError } from '../utils/error-handling/CustomErrors.js';
 
 const app = express();
 
 app.use(cors());
-
-connectDB().catch(() => {
-  throw new DatabaseError();
-});
 
 // Our httpServer handles incoming requests to our Express app.
 // Below, we tell Apollo Server to "drain" this httpServer,
@@ -45,25 +41,31 @@ await server.start();
 // and express middleware funtion
 
 app.use(
-  '/',
+  '/graphql',
   cors(),
   express.json(),
   // morgan('combined', { stream: accessLogStream }),
   // morgan(':method :url :status :res[content-length] - :response-time ms', { stream: new MongoDBStream() }),
   expressMiddleware(server, {
-    // context: ({ req }) => {
-    //   Logger.info(req.header('x-forwarded-for') || req.socket.remoteAddress);
-    //   const token = req.headers.authorization || '';
-    //   const authRepo = new AuthRepository(users);
-    //   const auth = new AuthServiceImplementation(authRepo, process.env.JWT_SECRET);
-    //   const currentUser = auth.verifyToken(token);
-    //   if (!currentUser) {
-    //     throw new AuthorizationError('Sem autorização. Faça login.');
-    //   }
-    //   return { currentUser };
-    // },
+    context: ({ req }) => {
+      return auth(req);
+    },
   }),
 );
+
+//testing middleware
+app.get('/test-error', (req, res, next) => {
+  throw new Error('Test Error');
+});
+
+app.use((err, req, res, next) => {
+  // Handle the error
+  console.log('Executing error handling middleware');
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+connectDB().catch(() => {
+  throw new DatabaseError();
+});
 
 // modify server startup
 await new Promise((resolve) => httpServer.listen({ port: process.env.PORT || 5001 }, resolve));
