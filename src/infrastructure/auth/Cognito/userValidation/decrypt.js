51a7bf84a0026576014584c4bf55098333ccedc7
pafm.iefp <pafm.iefp@alunos.ipb.pt>
2024-02-06 17:21:40 +0000

    decrypt changes

--- src/infrastructure/auth/Cognito/userValidation/decrypt.js
+++ src/infrastructure/auth/Cognito/userValidation/decrypt.js
@@ -1,14 +1,15 @@
 import crypto from 'crypto';
-import { getSecrets } from './secretsManager.js';
-import { SECRETS } from '../../../../utils/enums/enums.js';
+//import { getSecrets } from './secretsManager.js';
+//import { SECRETS } from '../../../../utils/enums/enums.js';
 
+const privateKey = process.env.privateKeyBackend;
 // Decrypting data from input
 const decryptingPassword = async function (input) {
-  const { password, email } = input;
+  //console.log('input', input);
+  const encryptedDataBuffer = Buffer.from(input.password, 'base64');
+  const { email } = input;
   try {
     // get the privateKey from aws
-
-    const privateKey = await getSecrets(SECRETS.PRIVATE_KEY);
 
     // Creating a decryption based on privateKey and the password
     const decryptedPassword = crypto.privateDecrypt(
@@ -21,7 +22,7 @@ const decryptingPassword = async function (input) {
         /* padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
         oaepHash: 'sha256', */
       },
-      password,
+      encryptedDataBuffer,
     );
 
     // Sending the correct object response with password decrypted
@@ -30,6 +31,7 @@ const decryptingPassword = async function (input) {
       email,
       password: decryptedPassword.toString('utf8'),
     };
+    console.log('-------------------', decryptedDataResponse);
     return decryptedDataResponse;
   } catch (e) {
     throw new Error('Error decrypting data');
