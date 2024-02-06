8e3ae1a686e05b8dd7e4100e6c5ceda8cdee2b12
Nessvah <info@silviavanessa.dev>
2024-02-06 09:45:38 +0000

    update script for deployment

--- scripts/deploy_to_ecr.sh
+++ scripts/deploy_to_ecr.sh
@@ -3,7 +3,9 @@
 DOCKER_IMAGE_NAME="icarus-api"
 ECR_REPO_URI="352074856451.dkr.ecr.eu-north-1.amazonaws.com/icarus-fast-api"
 AWS_REGION="eu-north-1"
+AWS_PROFILE_NAME="icarus-dev"
 DOCKERFILE_PATH="../src/Dockerfile"
+
 
 #Build first the docker image
 docker build -t $DOCKER_IMAGE_NAME -f $DOCKERFILE_PATH ../src
@@ -12,5 +14,5 @@ docker build -t $DOCKER_IMAGE_NAME -f $DOCKERFILE_PATH ../src
 docker tag $DOCKER_IMAGE_NAME:latest $ECR_REPO_URI:latest
 
 #Login to AWS ECR and push the docker image there
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REPO_URI
+aws ecr get-login-password --profile $AWS_PROFILE_NAME --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REPO_URI
 docker push $ECR_REPO_URI:latest
