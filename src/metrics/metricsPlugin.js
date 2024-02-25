c2712fc346f79e3611490ac32d657321a9a883b6
Nessvah <info@silviavanessa.dev>
2024-02-25 03:14:34 +0000

    removed introspection from metrics

--- src/metrics/metricsPlugin.js
+++ src/metrics/metricsPlugin.js
@@ -41,12 +41,6 @@ export function createMetricsPlugin(register) {
       ['operationName', 'operation'],
       register,
     ),
-    responded: createCounter(
-      'graphql_queries_responded',
-      'The amount of GraphQL queries that have been executed and been attempted to send to the client. This includes requests with errors.',
-      ['operationName', 'operation'],
-      register,
-    ),
     resolutionTime: createHistogram(
       'graphql_resolution_time',
       'The time taken to resolve a GraphQL query (in seconds).',
@@ -65,43 +59,36 @@ export function createMetricsPlugin(register) {
   // apollo have 9 steps in their whole lifecycle
 
   const metricsPlugin = {
-    async requestDidStart() {
-      return {
-        parsingDidStart(parsingContext) {
-          const labels = createLabels(parsingContext);
-          metrics.parsed.labels(labels).inc();
-          if (parsingContext.request.operationName !== 'IntrospectionQuery') logger.warn('parsing started 1');
-        },
-        async validationDidStart(validationContext) {
-          const labels = createLabels(validationContext);
-          metrics.validationStarted.labels(labels).inc();
-          if (validationContext.request.operationName !== 'IntrospectionQuery') logger.warn('validation started 2');
-        },
-        async didResolveOperation(resolveContext) {
-          // record start time for resolving the operation
-          // resolveContext.request.resolveStartTime = startTimer();
+    async requestDidStart(requestContext) {
+      const isIntrospection = checkForIntrospection(requestContext);
+      if (!isIntrospection) {
+        return {
+          parsingDidStart(_) {},
+          async validationDidStart(validationContext) {
+            const labels = createLabels(validationContext);
+            metrics.validationStarted.labels(labels).inc();
+          },
+          async didResolveOperation(resolveContext) {
+            // record start time for resolving the operation
+            // resolveContext.request.resolveStartTime = startTimer();
 
-          const labels = createLabels(resolveContext);
-          metrics.resolved.labels(labels).inc();
-          if (resolveContext.request.operationName !== 'IntrospectionQuery') logger.warn('did resolve started 3');
-        },
-        async executionDidStart(executingContext) {
-          // track current time for the execution and
-          // attach the start time to the context in the req obj
+            const labels = createLabels(resolveContext);
+            metrics.resolved.labels(labels).inc();
+          },
+          async executionDidStart(executingContext) {
+            // track current time for the execution and
+            // attach the start time to the context in the req obj
 
-          executingContext.request.startTime = startTimer();
+            executingContext.request.startTime = startTimer();
 
-          const labels = createLabels(executingContext);
-          metrics.startedExecuting.inc(labels);
-          if (executingContext.request.operationName !== 'IntrospectionQuery') logger.warn('execution did started 4');
-        },
-        async didEncounterErrors(errorContext) {
-          const labels = createLabels(errorContext);
-          metrics.encounteredErrors.labels(labels).inc();
-          if (errorContext.request.operationName !== 'IntrospectionQuery') logger.warn('did encountered errors 5');
-        },
-        async willSendResponse(responseContext) {
-          if (responseContext.request.operationName !== 'IntrospectionQuery') {
+            const labels = createLabels(executingContext);
+            metrics.startedExecuting.inc(labels);
+          },
+          async didEncounterErrors(errorContext) {
+            const labels = createLabels(errorContext);
+            metrics.encounteredErrors.labels(labels).inc();
+          },
+          async willSendResponse(responseContext) {
             const { startTime } = responseContext.request;
             const end = startTimer();
             try {
@@ -110,25 +97,20 @@ export function createMetricsPlugin(register) {
               const duration = getDurationInSecs(startTime, end);
               logger.warn('will send response 6');
               metrics.resolutionTime.observe(labels, duration);
+              metrics.responded.labels(labels).inc();
             } catch (err) {
               console.log(err, 'error');
             }
-          }
-
-          // get the start time from request
-          // const { startTime, resolveStartTime } = responseContext.request;
-          // // get current time
-          // const endTime = startTimer();
-          // const executionTime = getDurationInSecs(startTime, endTime);
-          // const resolutionTime = getDurationInSecs(resolveStartTime, endTime);
-
-          // metrics.executionTime.observe(labels, executionTime);
-          // metrics.resolutionTime.observe(labels, resolutionTime);
-          // metrics.responded.labels(labels).inc();
-        },
-      };
+          },
+        };
+      }
     },
   };
   // return the plugin to integrate with apollo
   return metricsPlugin;
 }
+
+const checkForIntrospection = (context) => {
+  if (context.request.operationName === 'IntrospectionQuery') return true;
+  return false;
+};
