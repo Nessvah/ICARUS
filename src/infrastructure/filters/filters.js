b0873c5a71810bb557dbb392bc0cedd5d9ceeae7
Susana <su401@MSI.MSI>
2024-02-26 11:31:38 +0000

    ready to merge mto main

--- src/infrastructure/filters/filters.js
+++
@@ -1,5 +0,0 @@
-// If database type is 'mysql', then adapt filter operators logic to sql (knext).
-
-// apply logical operators to filter values. - where + _and / _or
-
-//apply value operator to filter values. - where + _eq / _neq / _lt / _lte / _gt / _gte / _in / _nin
