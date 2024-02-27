a4e47b01c1e73c6954f7d1c264a6b0ba8ddfc74a
pafm.iefp <pafm.iefp@alunos.ipb.pt>
2024-02-27 11:17:47 +0000

    introspection fix for authentication

--- src/infrastructure/server.js
+++ src/infrastructure/server.js
@@ -81,6 +81,9 @@ app.use(
   express.json(),
   expressMiddleware(server, {
     context: ({ req }) => {
+      if (req.body.operationName === 'IntrospectionQuery') {
+        return { req };
+      }
       return auth(req);
     },
   }),
