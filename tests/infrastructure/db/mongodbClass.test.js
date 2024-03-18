0795f4eec3207e18ae3e3ccaf04b3b2f1b4c3212
Eliana Sofia Serrano Delgado <essd.iefp@alunos.ipb.pt>
2024-03-18 11:42:07 +0000

    connector test + tests refactoring according new structure (icarus-core + icarus-server)

---
+++ tests/infrastructure/db/mongodbClass.test.js
@@ -0,0 +1,73 @@
+import { MongoDBConnection } from '../../../icarus-core/infrastructure/db/mongodbClass';
+import { logger } from '../../../icarus-core/infrastructure/server';
+
+// Mock the ImportThemTities class
+jest.mock('../../../icarus-core/config/importDemTities.js', () => ({
+  ImportThemTities: jest.fn().mockImplementation(() => ({
+    // Mock the importAll method
+    importAll: jest.fn().mockResolvedValue({
+      tables: [], // Mock the tables array
+      connections: [], // Mock the connections array
+    }),
+  })),
+}));
+
+// Mock logger
+jest.mock('../../../icarus-core/infrastructure/server', () => ({
+  logger: {
+    error: jest.fn(),
+  },
+}));
+
+// Mock MongoDB pool object
+const pool = {
+  connect: jest.fn(),
+  close: jest.fn(),
+  db: jest.fn(),
+};
+
+// Mock currentTableInfo object
+const currentTableInfo = {
+  table: 'testTable',
+  type: 'mongo',
+  databaseName: 'testDB',
+  columns: {},
+  pool: pool,
+};
+
+// MongoDBConnection object for testing
+const mongoDBConnection = new MongoDBConnection(currentTableInfo);
+
+describe('MongoDBConnection', () => {
+  // CONNECTION
+
+  describe('connect', () => {
+    it('should connect to the MongoDB database', async () => {
+      await mongoDBConnection.connect();
+      expect(pool.connect).toHaveBeenCalled();
+    });
+
+    it('should log an error if connection fails', async () => {
+      pool.connect.mockRejectedValueOnce(new Error('Connection error'));
+      await mongoDBConnection.connect();
+      expect(logger.error).toHaveBeenCalledWith('Error to connect to MongoDB:', new Error('Connection error'));
+    });
+  });
+
+  // Test the close method
+  describe('close', () => {
+    it('should close the connection to the MongoDB database', async () => {
+      await mongoDBConnection.close();
+      expect(pool.close).toHaveBeenCalled();
+    });
+
+    it('should log an error if closing connection fails', async () => {
+      pool.close.mockRejectedValueOnce(new Error('Close connection error'));
+      await mongoDBConnection.close();
+      expect(logger.error).toHaveBeenCalledWith(
+        'Error closing MongoDB connection:',
+        new Error('Close connection error'),
+      );
+    });
+  });
+});
---
+++ tests/infrastructure/db/mysqlClass.test.js
@@ -0,0 +1,51 @@
+import { MySQLConnection } from '../../../icarus-core/infrastructure/db/mysqlClass';
+import { logger } from '../../../icarus-core/infrastructure/server';
+
+// Mock the ImportThemTities class
+jest.mock('../../../icarus-core/config/importDemTities.js', () => ({
+  ImportThemTities: jest.fn().mockImplementation(() => ({
+    // Mock the importAll method
+    importAll: jest.fn().mockResolvedValue({
+      tables: [], // Mock the tables array
+      connections: [], // Mock the connections array
+    }),
+  })),
+}));
+
+jest.mock('../../../icarus-core/infrastructure/server', () => ({
+  logger: {
+    warn: jest.fn(),
+    error: jest.fn(),
+  },
+}));
+
+// Mock MySQL pool object
+const pool = {
+  getConnection: jest.fn(),
+};
+
+// Mock currentTableInfo object
+const currentTableInfo = {
+  table: 'testTable',
+  pool: pool,
+};
+
+// MySQLConnection object for testing
+const mySQLConnection = new MySQLConnection(currentTableInfo);
+
+describe('MySQLConnection', () => {
+  describe('getConnection', () => {
+    it('should log a warning message when getting a connection', async () => {
+      await mySQLConnection.getConnection();
+      expect(logger.warn).toHaveBeenCalledWith('getting mysql conn from pool...');
+    });
+
+    it('should return a connection from the pool', async () => {
+      const connection = {};
+      pool.getConnection.mockResolvedValueOnce(connection);
+
+      const result = await mySQLConnection.getConnection();
+      expect(result).toBe(connection);
+    });
+  });
+});
