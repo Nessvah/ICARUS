e77361410b1509c3ccda434e70222a3cbe90fdf2
Silvia Vanessa <info@silviavanessa.dev>
2024-03-18 13:52:43 +0000

    return authentication

--- src/infrastructure/server.js
+++ src/infrastructure/server.js
@@ -91,7 +91,7 @@ app.use(
       if (req.body.operationName === 'IntrospectionQuery') {
         return { req };
       } else {
-        return { req };
+        return auth(req);
       }
     },
   }),
